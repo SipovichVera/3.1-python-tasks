5bb91ef5f767bed612fead706c721750733f3e58
Vera <Vera.Sipovich@atechart-group.com>
2022-01-17 13:43:40 +0300

    change task1

--- task1.py
+++ task1.py
@@ -1,6 +1,12 @@
-my_dict = {'a':1, 'b':2, 'c':3}
-new_dict = my_dict.copy()
-for key, value in new_dict.items():
-    new_dict[key] = key
-    new_dict[value] = new_dict.pop(key)
-print(new_dict)
+def copy_dict():
+    my_dict = {'a':1, 'b':2, 'c':3}
+    return my_dict.copy()
+
+def swap_keys_values():
+    new_dict = copy_dict()
+    for key, value in new_dict.items():
+        new_dict[key] = key
+        new_dict[value] = new_dict.pop(key)
+    return new_dict
+
+__main__ = print(swap_keys_values())
