8cec639f5492a179b3fe3ad8980adf9379a42143
Vera <Vera.Sipovich@atechart-group.com>
2022-01-17 17:06:50 +0300

    task5

--- task5.py
+++ task5.py
@@ -1,11 +1,12 @@
 def summa_of_number(*args):
     summa = 0
-    try:
-        for item in args:
+    for item in args:
+        try:
             if type(item) != int:
                 raise TypeError
             summa += item
-    finally:
-        print("end")
+        finally:
+            continue
+    return summa
 
-print(summa_of_number(1,2,'vde',4))
+print(summa_of_number(1,2,'vde',4, 10, 2.2, 'a'))
